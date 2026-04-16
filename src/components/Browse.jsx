b466b4de1a2243b0c9a25b3fdfbff262f147df01
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2026-04-16 22:48:44 +0530

    Enhanced Modularity

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,26 +1,15 @@
-import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/Constants";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies} from  "../utils/moviesSlice"
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+
+
+
 const Browse = () => {
-  const dispatch = useDispatch()
 
 
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS,
-    );
 
-    const json = await data.json();
-    dispatch(addNowPlayingMovies(json.results))
-    
-  };
 
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+   
+  useNowPlayingMovies();
 
   return (
     <div>

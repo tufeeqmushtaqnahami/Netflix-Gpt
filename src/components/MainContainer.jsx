98422809a1a2b5db4116690baf1035afedb51dcf
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2026-04-18 16:11:16 +0530

    Created the Reduxstore for Trailer part of browse section and started the styling of the Browse Page

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -8,14 +8,18 @@ const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies)
 
   if(!movies) return; 
-
+ 
   const mainMovie = movies[0]
   console.log(mainMovie)
 
+  const {original_title, overview , id }= mainMovie 
+  console.log(original_title)
+  console.log(overview)
+
   return (
     <div>
-      <VideoTitle/>
-      <VideoBackgorund/>
+      <VideoTitle title={original_title} overview={overview}/>
+      <VideoBackgorund movieID={id}/>
     </div>
   )
 }
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const VideoTitle = () => {
+const VideoTitle = ({title , overview}) => {
   return (
-    <div>VideoTitle</div>
+    <div className='pt-36 px-20'>
+      <h1 className='text-6xl font-bold'>{title}</h1>
+      <p className='py-6 text-lg w-1/3'>{overview}</p>
+
+
+
+      <div className=''>
+        <button className='bg-gray-600  px-12 text-xl text-white p-4  bg-opacity-50 rounded-lg'>▶️ PLay</button>
+        <button className='bg-gray-800   px-12 text-xl text-white p-4  bg-opacity-50 rounded-lg mx-5'>More Info</button>
+      </div>
+
+
+
+    </div>
   )
 }
 
